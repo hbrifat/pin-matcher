40b0fb3cee366b95ebdea097cfed742c70f5d9f0
hb rifat <hbrifat@gmail.com>
2020-08-03 21:22:14 +0600

    js file added

---
+++ script.js
@@ -0,0 +1,37 @@
+const pinButton = document.getElementById("pinButton").addEventListener("click", function(){
+  document.getElementById("generatePinDisplay").value = Math.round(Math.random() * 10000 );
+})
+
+document.getElementById("one").addEventListener("click", function(){
+   document.getElementById("inputPinDisplay").value += '1';
+})
+document.getElementById("two").addEventListener("click", function(){
+   document.getElementById("inputPinDisplay").value += '2';
+})
+document.getElementById("three").addEventListener("click", function(){
+   document.getElementById("inputPinDisplay").value += '3';
+})
+document.getElementById("four").addEventListener("click", function(){
+   document.getElementById("inputPinDisplay").value += '4';
+})
+document.getElementById("five").addEventListener("click", function(){
+   document.getElementById("inputPinDisplay").value += '5';
+})
+document.getElementById("six").addEventListener("click", function(){
+   document.getElementById("inputPinDisplay").value += '6';
+})
+document.getElementById("seven").addEventListener("click", function(){
+   document.getElementById("inputPinDisplay").value += '7';
+})
+document.getElementById("eight").addEventListener("click", function(){
+   document.getElementById("inputPinDisplay").value += '8';
+})
+document.getElementById("nine").addEventListener("click", function(){
+   document.getElementById("inputPinDisplay").value += '9';
+})
+document.getElementById("zero").addEventListener("click", function(){
+   document.getElementById("inputPinDisplay").value += '0';
+})
+document.getElementById("clear").addEventListener("click", function(){
+   document.getElementById("inputPinDisplay").value = '';
+})
